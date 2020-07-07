5a9159ac6019e422a608342d50230cb7f68d7025
Rajashree <rhalingale@gmail.com>
2020-07-06 21:50:09 -0400

    update functional hands on

---
+++ index.js
@@ -0,0 +1,15 @@
+function calculateTotal(items) {
+  items.forEach(item => {
+    Calculate(item)
+  })
+
+  return items
+}
+
+function Calculate(item) {
+  item.total = parseFloat(((item.price - (item.price * item.discount)) * item.quantity).toFixed(2))
+
+  return item
+}
+
+module.exports = calculateTotal
